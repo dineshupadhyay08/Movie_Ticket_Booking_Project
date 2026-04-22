be4b3a409cf5e15f5f9dab02a76544be1231184a
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-04-23 00:28:06 +0530

    Add Movies suggestion sectin

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -1,30 +1,38 @@
 import { ArrowRightIcon } from 'lucide-react'
 import React from 'react'
-import BlurCircle from './BlurCircle'
-import { Navigate } from 'react-router'
-import MovieCard from './MovieCard'
+import { useNavigate } from 'react-router-dom'
 import { dummyShowsData } from '../assets/assets'
+import BlurCircle from './BlurCircle'
+import MovieCard from './MovieCard'
 
 const FeaturedSection = () => {
-  return (
-    <div className='px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden'>
-      <div className='relative flex items-center justify-between pt-20 pb-10'>
-        <BlurCircle top='0' right='-80px'/>
-        <p className='text-gray-300 font-medium text-lg'>Now Showing</p>
-        <button className='group flex items-center gap-2 text-sm text-gray-300'>
-          View All 
-          <ArrowRightIcon className='group-hover:translate-x-0.5 transition w-4.5 h-4.5'/>
+  const navigate = useNavigate()
 
-         </button>
+  return (
+    <div className='overflow-hidden px-6 md:px-16 lg:px-24 xl:px-44'>
+      <div className='relative flex items-center justify-between pt-20 pb-10'>
+        <BlurCircle top='0' right='-80px' />
+        <p className='text-lg font-medium text-gray-300'>Now Showing</p>
+        <button className='group flex items-center gap-2 text-sm text-gray-300'>
+          View All
+          <ArrowRightIcon className='h-4.5 w-4.5 transition group-hover:translate-x-0.5' />
+        </button>
       </div>
-      <div>
-        {dummyShowsData.slice(0, 4).map((show)=>(
-          <MovieCard key={show._id} movie={show}/>
+      <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+        {dummyShowsData.slice(0, 4).map((show) => (
+          <MovieCard key={show._id} movie={show} />
         ))}
       </div>
-      <div className='flex justify-center mt-20'>
-        <button onClick={()=> {Navigate('/movies'); scrollTo(0,0)}}
-        className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer'>Show more</button>
+      <div className='mt-20 flex justify-center'>
+        <button
+          onClick={() => {
+            navigate('/movies')
+            scrollTo(0, 0)
+          }}
+          className='cursor-pointer rounded-md bg-primary px-10 py-3 text-sm font-medium transition hover:bg-primary-dull'
+        >
+          Show more
+        </button>
       </div>
     </div>
   )
--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,41 +1,46 @@
 import React from 'react'
 import { StarIcon } from 'lucide-react'
-import { useNavigate } from 'react-router'
+import { useNavigate } from 'react-router-dom'
 
-const MovieCard = ({movie}) => {
-
+const MovieCard = ({ movie }) => {
   const navigate = useNavigate()
-  
+
   return (
-    <div className="flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:-translate-y-1 transition duration-300 w-66">
+    <div className="flex w-66 flex-col justify-between rounded-2xl bg-gray-800 p-3 transition duration-300 hover:-translate-y-1">
       <img
         onClick={() => {
-          navigate(`/movies/${movie._id}`);
-          scrollTo(0, 0);
+          navigate(`/movies/${movie._id}`)
+          scrollTo(0, 0)
         }}
         src={movie.backdrop_path}
-        className="rounded-lg h-52 w-full object-cover object-right-bottom cursor-pointer"
+        className="h-52 w-full cursor-pointer rounded-lg object-cover object-right-bottom"
       />
-      <p className="font-semibold mt-2 truncate">{movie.title}</p>
-      <p className="text-sm text-gray-400 mt-2">
-        {new Date(movie.release_data).getFullYear()} •{" "}
+      <p className="mt-2 truncate font-semibold">{movie.title}</p>
+      <p className="mt-2 text-sm text-gray-400">
+        {new Date(movie.release_date).getFullYear()} {" • "}
         {movie.genres
           .slice(0, 2)
           .map((genre) => genre.name)
           .join(" | ")}{" "}
-        • {movie.runtime}
+        {" • "} {movie.runtime}
       </p>
-      <div className='flex items-center justify-between mt-4 pb-3'>
-        <button onClick={()=> {navigate(`/movies/${movie._id}`); scrollTo(0,0)}}
-        className='px-4 py-2 text-xs bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Buy Tickets</button>
-        <p className='flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1'>
-          <StartIcon className='w-4 h-4 text-primary fill-primary'/>
+      <div className='mt-4 flex items-center justify-between pb-3'>
+        <button
+          onClick={() => {
+            navigate(`/movies/${movie._id}`)
+            scrollTo(0, 0)
+          }}
+          className='cursor-pointer rounded-full bg-primary px-4 py-2 text-xs font-medium transition hover:bg-primary-dull'
+        >
+          Buy Tickets
+        </button>
+        <p className='mt-1 flex items-center gap-1 pr-1 text-sm text-gray-400'>
+          <StarIcon className='h-4 w-4 fill-primary text-primary' />
           {movie.vote_average.toFixed(1)}
         </p>
       </div>
-      
     </div>
-  );
+  )
 }
 
 export default MovieCard

cf71d44e966d33ead9fbf0c6fff70318fe90bb4a
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-04-25 19:23:51 +0530

    Fix the Video Player on Screen

--- client/src/components/TrailerSection.jsx
+++ client/src/components/TrailerSection.jsx
@@ -1,21 +1,32 @@
 import React, { useState } from 'react'
-import {dummyTrailers} from '../assets/assets'
+import { dummyTrailers } from '../assets/assets'
 import ReactPlayer from 'react-player'
 import BlurCircle from './BlurCircle'
 
 const TrailerSection = () => {
-  const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[0])
+  const [currentTrailer] = useState(dummyTrailers[0])
+
   return (
-    <div className='px-6 md:px-16 lg:px-24 xl:h-44 py-20 overflow-hidden'>
-      <p className='text-gray-300 font-medium text-lg max-w-[960] mx-auto'>
+    <section className='px-6 py-20 md:px-16 lg:px-24 xl:px-44'>
+      <p className='mx-auto text-lg font-medium text-gray-300'>
         Trailers
       </p>
-      <div className='relative mt-6'>
-        <BlurCircle top="-100" right="-100px"/>
-        <ReactPlayer url={currentTrailer.videoUrl} controls={false} 
-        className='mx-auto max-w-full' width="960px" height="540px"/>
+
+        <BlurCircle top='-100' right='50px' />
+      <div className='relative mx-auto mt-6 w-full  overflow-hidden rounded-2xl'>
+
+        <div className='aspect-video w-full'>
+          <ReactPlayer
+            src={currentTrailer.videoUrl}
+            controls
+            playsInline
+            width='100%'
+            height='100%'
+            className='mx-auto max-w-full'
+          />
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 

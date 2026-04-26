2522ea45f7979daa30d9627662e62ce301dae55b
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-04-26 23:54:58 +0530

    Add Movies Section

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { dummyShowsData } from "../assets/assets.js";
 import MovieCard from '../components/MovieCard'
+import BlurCircle from '../components/BlurCircle.jsx';
 
 const Movies = () => {
   return dummyShowsData.length > 0 ? (
     <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[8vh]'>
+      <BlurCircle top='150px' left='0'/>
+      <BlurCircle bottom='50px' right='50'/>
       <h1 className='text-lg font-medium my-4'>Now Showing</h1>
       <div className='flex flex-wrap max-sm:justify-center gap-8'>
         {dummyShowsData.map((movie) => (

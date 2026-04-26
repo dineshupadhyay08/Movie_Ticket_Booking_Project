6169e6a19370ec734f0d1e729d796973eb725f1c
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-04-27 00:25:59 +0530

    Add Movies Full Details Section

--- client/src/pages/MoviesDetails.jsx
+++ client/src/pages/MoviesDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 
@@ -13,11 +13,18 @@ const MoviesDetails = () => {
       dateTime: dummyDateTimeData
     })
   }
-  return (
-    <div>
-      
+
+  useEffect(() => {
+    getShow()
+  }, [id])
+
+  return show ? (
+    <div className='px-6 md:px-16 lg:px-40 pt-30 md:pt-50'>
+      <div>
+        <img src={show.movie.poster_path} alt='' className=''/>
+      </div>
     </div>
-  )
+  ) :  <div>Loading...</div>
 }
 
 export default MoviesDetails

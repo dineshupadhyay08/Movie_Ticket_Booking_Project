45274a57c98e6615549861b2724d6765c8905249
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-04-26 23:51:02 +0530

    Add Movies Section

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -5,8 +5,8 @@ import MovieCard from '../components/MovieCard'
 const Movies = () => {
   return dummyShowsData.length > 0 ? (
     <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[8vh]'>
-      <h1>Now Showing</h1>
-      <div>
+      <h1 className='text-lg font-medium my-4'>Now Showing</h1>
+      <div className='flex flex-wrap max-sm:justify-center gap-8'>
         {dummyShowsData.map((movie) => (
           <MovieCard movie={movie} key={movie._id} />
         ))}

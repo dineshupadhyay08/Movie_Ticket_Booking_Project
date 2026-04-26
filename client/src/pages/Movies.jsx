73f31b822cc9169449127ab59ac4986a145794c3
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-04-26 23:45:24 +0530

    Add Movies Section

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React from 'react'
+import { dummyShowsData } from "../assets/assets.js";
+import MovieCard from '../components/MovieCard'
 
 const Movies = () => {
-  return <div></div>;
-};
+  return dummyShowsData.length > 0 ? (
+    <div>
+      <h1>Now Showing</h1>
+      <div>
+        {dummyShowsData.map((movie) => (
+          <MovieCard movie={movie} key={movie._id} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div>
 
-export default Movies;
+    </div>
+  )
+}
+
+export default Movies

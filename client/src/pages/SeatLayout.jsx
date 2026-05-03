d4786c26247ec82efe3d191d8ecef03249ffa7b4
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-03 23:59:29 +0530

    Add Movie Booking Layout

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -34,12 +34,17 @@ const SeatLayout = () => {
       <div className="w-60 bg-primary/10 border-primary/20 rounded-lg py-10 h-max md:sticky md:to-pink-30">
         <p className="text-lg font-semibold px-6">Available Timings</p>
       </div>
-      {Show.dateTime[date].map((time) => (
+      {Show?.dateTime?.[date]?.map((time, index) => (
         <div
-          className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${selectedTime?.time === item.time ? "bg-primary text-white" : "hover:bg-primary/20"}`}
+          key={index}
+          className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${
+            selectedTime?.time === time.time
+              ? "bg-primary text-white"
+              : "hover:bg-primary/20"
+          }`}
         >
           <ClockIcon className="w-4 h-4" />
-          <p className="text-sm">{item.time}</p>
+          <p className="text-sm">{time.time}</p>
         </div>
       ))}
     </div>

64a55a0cc93532d96bbc86c188a3e635d61fa4d1
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-03 23:32:50 +0530

    Add Loading Section

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { dummyShowsData } from '../assets/assets'
+import Loading from '../components/Loading'
 
 const SeatLayout = () => {
 
@@ -6,13 +8,30 @@ const SeatLayout = () => {
   const [selectedSeats, setSelectedSeats] = useState([])
   const [selectedTime, setSelectedTime] = useState(null)
   const [Show, setShow] = useState(null)
+
   const navigate = useNavigate()
 
+  const getShow = async () => {
+    const show = dummyShowsData.find(show => show._id)
+    if (show) {
+      setShow({
+        movie: show,
+        dateTime: dummyDateTimeData,
 
-  return (
-    <div>
+      })
+    }
+  }
+
+  useEffect(() => {
+    getShow()
+  },[])
+
+  return show ? (
+    <div className='flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50'>
       
     </div>
+  ) : (
+    <Loading/>
   )
 }
 

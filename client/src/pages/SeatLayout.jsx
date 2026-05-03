2afa7f4b789aefc6c7fd75b8f336ccf85362e34c
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-03 23:58:00 +0530

    Add Movie Booking Layout

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react'
-import { dummyShowsData } from '../assets/assets'
+import React, { useEffect, useState } from 'react'
+import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import { div } from 'framer-motion/client'
 import { ClockIcon } from 'lucide-react'
+import { useNavigate, useParams } from 'react-router'
 
 const SeatLayout = () => {
 
@@ -28,22 +29,23 @@ const SeatLayout = () => {
     getShow()
   },[])
 
-  return show ? (
-    <div className='flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50'>
-      <div className='w-60 bg-primary/10 border-primary/20 rounded-lg py-10 h-max md:sticky md:to-pink-30'>
-      <p className='text-lg font-semibold px-6'>Available Timings</p>
+  return Show ? (
+    <div className="flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50">
+      <div className="w-60 bg-primary/10 border-primary/20 rounded-lg py-10 h-max md:sticky md:to-pink-30">
+        <p className="text-lg font-semibold px-6">Available Timings</p>
       </div>
-      {show.dateTime[date].map((time) => (
-        <div className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${selectedTime?.time === item.time ? "bg-primary text-white" : "hover:bg-primary/20"}`}>
-          <ClockIcon className='w-4 h-4'/>
-          <p className='text-sm'>{item.time}</p>
+      {Show.dateTime[date].map((time) => (
+        <div
+          className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${selectedTime?.time === item.time ? "bg-primary text-white" : "hover:bg-primary/20"}`}
+        >
+          <ClockIcon className="w-4 h-4" />
+          <p className="text-sm">{item.time}</p>
         </div>
       ))}
-      
     </div>
   ) : (
-    <Loading/>
-  )
+    <Loading />
+  );
 }
 
 export default SeatLayout

15eb1e452b204c28e9461e3c4162b950215cdd84
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-10 19:36:48 +0530

    Fix Error in Admin Sidebar

--- client/src/components/Admin/AdminSidebar.jsx
+++ client/src/components/Admin/AdminSidebar.jsx
@@ -45,11 +45,7 @@ const AdminSidebar = () => {
                 <link.icon className="w-5 h-5" />
                 <p className="max-md:hidden">{link.name}</p>
 
-                <span
-                  className={`w-1.5 h-10 rounded-l right-0 absolute ${
-                    isActive && "bg-primary"
-                  }`}
-                ></span>
+                
               </>
             )}
           </NavLink>

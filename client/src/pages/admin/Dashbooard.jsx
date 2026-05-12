86bae3d59a6b6f5aaa7e763d35128a1b3a3aa450
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-12 23:46:04 +0530

    Add Admin Dashboard

--- client/src/pages/admin/Dashbooard.jsx
+++ client/src/pages/admin/Dashbooard.jsx
@@ -42,6 +42,11 @@ const Dashbooard = () => {
       <div className='flex flex-wrap gap-4 w-full'>
         {dashboardCards.map((card, index) =>(
           <div key={index} className='flex items-center justify-between px-4 py-3 bg-primary/10 border border-primary/20 rounded-md max-w-50 w-full'>
+            <div>
+              <h1 className='text-sm'>{card.title}</h1>
+              <p className='text-xl font-medium mt-1'>{card.value}</p>
+            </div>
+            <card.icon className='w-6 h-6'/>
 
           </div>
         ))}

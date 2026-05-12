4f5dc9f8e4e75739184efa81c5d15b0ef31f430d
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-12 23:57:19 +0530

    Add Admin Dashboard

--- client/src/pages/admin/Dashbooard.jsx
+++ client/src/pages/admin/Dashbooard.jsx
@@ -1,4 +1,4 @@
-import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, UserIcon } from 'lucide-react'
+import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, StarIcon, UserIcon } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 import { dummyDashboardData } from '../../assets/assets'
 import Loading from '../../components/Loading'
@@ -61,8 +61,12 @@ const Dashbooard = () => {
         <div key={show.id} className='w-55 rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border border-primary/20 hover:-translate-y-1 transition duration-300'>
           <img src={show.movie.poster_path} alt='' className='h-60 w-full object-cover'/>
           <p className='font-medium p-2 truncate'>{show.movie.title}</p>
-          <div>
-            
+          <div className='flex items-center justify-between px-2'>
+            <p className='text-lg font-medium'>{currency} {show.showPrice}</p>
+            <p className='flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1'>
+              <StarIcon className='w-4 h-4 text-pretty fill-primary'/>
+              {show.movie.vote_average.toFixed(1)}
+            </p>
           </div>
 
         </div>

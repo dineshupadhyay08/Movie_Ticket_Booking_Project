481454ad63a6a103fbb53845b78094bd1c454b6c
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-12 23:28:01 +0530

    Add Admin Dashboard

--- client/src/pages/admin/Dashbooard.jsx
+++ client/src/pages/admin/Dashbooard.jsx
@@ -1,3 +1,4 @@
+import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, UserIcon } from 'lucide-react'
 import React, { useState } from 'react'
 
 const Dashbooard = () => {
@@ -11,7 +12,15 @@ const Dashbooard = () => {
     totalUser : 0
   })
 
-  
+  const [loading,setLoading] = useState(true)
+
+  const dashboardCards = [
+    {title: "Total Booking", value: dashboardData.totalBookings || "0", icon: ChartLineIcon},
+    {title: "Total Revenue", value: dashboardData.totalRevenue || "0", icon: CircleDollarSignIcon},
+    {title: "Active Shows", value: dashboardData.activeShows.length || "0", icon: PlayCircleIcon},
+    {title: "Total Users", value: dashboardData.totalUser || "0", icon: UserIcon},
+  ]
+
 
   return (
     <div>

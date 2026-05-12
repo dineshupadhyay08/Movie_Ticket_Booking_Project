491204ffd45e39fb3a225c94d49b7688159d4309
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-12 23:59:38 +0530

    Add Admin Dashboard

--- client/src/pages/admin/Dashbooard.jsx
+++ client/src/pages/admin/Dashbooard.jsx
@@ -36,45 +36,59 @@ const Dashbooard = () => {
 
   return !loading ? (
     <>
-    <Title text1="Admin" text2="Dashboard"/>
-    <div className='relative flex flex-wrap gap-4 mt-6'>
-      <BlurCircle top='-100px' left='0'/>
-      <div className='flex flex-wrap gap-4 w-full'>
-        {dashboardCards.map((card, index) =>(
-          <div key={index} className='flex items-center justify-between px-4 py-3 bg-primary/10 border border-primary/20 rounded-md max-w-50 w-full'>
-            <div>
-              <h1 className='text-sm'>{card.title}</h1>
-              <p className='text-xl font-medium mt-1'>{card.value}</p>
+      <Title text1="Admin" text2="Dashboard" />
+      <div className="relative flex flex-wrap gap-4 mt-6">
+        <BlurCircle top="-100px" left="0" />
+        <div className="flex flex-wrap gap-4 w-full">
+          {dashboardCards.map((card, index) => (
+            <div
+              key={index}
+              className="flex items-center justify-between px-4 py-3 bg-primary/10 border border-primary/20 rounded-md max-w-50 w-full"
+            >
+              <div>
+                <h1 className="text-sm">{card.title}</h1>
+                <p className="text-xl font-medium mt-1">{card.value}</p>
+              </div>
+              <card.icon className="w-6 h-6" />
             </div>
-            <card.icon className='w-6 h-6'/>
+          ))}
+        </div>
+      </div>
+      <p className="mt-10 text-lg font-medium">Active Shows</p>
 
+      <div className="relative flex flex-wrap gap-6 mt-4 max-w-5xl">
+        <BlurCircle top="100px" left="-10" />
+
+        {dashboardData.activeShows.map((show) => (
+          <div
+            key={show.id}
+            className="w-55 rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border border-primary/20 hover:-translate-y-1 transition duration-300"
+          >
+            <img
+              src={show.movie.poster_path}
+              alt=""
+              className="h-60 w-full object-cover"
+            />
+
+            <p className="font-medium p-2 truncate">{show.movie.title}</p>
+
+            <div className="flex items-center justify-between px-2">
+              <p className="text-lg font-medium">
+                {currency} {show.showPrice}
+              </p>
+
+              <p className="flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1">
+                <StarIcon className="w-4 h-4 text-pretty fill-primary" />
+                {show.movie.vote_average.toFixed(1)}
+              </p>
+            </div>
           </div>
         ))}
-
       </div>
-
-    </div>
-    <p className='mt-10 text-lg font-medium'>Active Shows</p>
-    <div className='relative flex flex-wrap gap-6 mt-4 max-w-5xl'>
-      <BlurCircle top='100px' left='-10'/>
-      {dashboardCards.activeShows.map((shwo)=>(
-        <div key={show.id} className='w-55 rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border border-primary/20 hover:-translate-y-1 transition duration-300'>
-          <img src={show.movie.poster_path} alt='' className='h-60 w-full object-cover'/>
-          <p className='font-medium p-2 truncate'>{show.movie.title}</p>
-          <div className='flex items-center justify-between px-2'>
-            <p className='text-lg font-medium'>{currency} {show.showPrice}</p>
-            <p className='flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1'>
-              <StarIcon className='w-4 h-4 text-pretty fill-primary'/>
-              {show.movie.vote_average.toFixed(1)}
-            </p>
-          </div>
-
-        </div>
-      ))}
-
-    </div>
     </>
-  ) : <Loading/>
+  ) : (
+    <Loading />
+  );
 }
 
 export default Dashbooard

d4916bfc34e7f7a9ba1f629cc1272c166f29c95a
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-12 23:21:23 +0530

    Add Admin Dashboard

--- client/src/pages/admin/Dashbooard.jsx
+++ client/src/pages/admin/Dashbooard.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Dashbooard = () => {
+
+  const currency = import.meta.env.VITE_CURRENCY
+
+  const [dashboardData, setDashboardData] = useState({
+    totalBookings : 0,
+    totalRevenue: 0,
+    activeShows: [],
+    totalUser : 0
+  })
+
+  
+
   return (
     <div>
-      
+    
     </div>
   )
 }

9c036dfbf925573ff664bfe217a15d2215b2baa4
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-14 22:31:28 +0530

    Add List Show Section in Admin Pannel

--- client/src/pages/admin/ListShows.jsx
+++ client/src/pages/admin/ListShows.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { dummyShowsData } from '../../assets/assets'
 
 const ListShows = () => {
+
+  const currency = import.meta.env.VITE_CURRENCY
+
+  const [show,setShow] = useState([])
+  const [loading,setLoading] = useState(true)
+
+  const getAllShow = async () =>{
+    try {
+      setShow([{
+        movie: dummyShowsData[0],
+        showDateTime: "2025-06-30T02:30:00.000Z",
+        showPrice: 59,
+        occupiedSeats: {
+          A1: "user_1",
+          B1: "user_2",
+          C1: "user_3"
+        }
+      }])
+    }
+  }
+
+
   return (
     <div>
       

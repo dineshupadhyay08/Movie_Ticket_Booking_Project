de9412c69332a9f6a0be6c63c15597e2ff03aa5a
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-16 23:45:56 +0530

    Add AddShow Section

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddShows = () => {
+  const currency = import.meta.env.VITE_CURRENCY
+  const [nowPlayingMovies,setNowPlayingMovies] = useState([]);
+  const [selectedMovie,setSelectedMovie] = useState(null);
+  const [dateTimeSelection,setDateTimeSelection] = useState({});
+  const [dateTimeInput,setDateTimeInput] = useState("");
+  const [showPrice,setShowPrice] = useState("");
+  
   return (
     <div>
       

08074cfbe72b27f48a3d10d9981ac8ce7d03d39f
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-17 22:37:05 +0530

    Add Add Show Section in Admin Side

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -5,6 +5,7 @@ import Title from '../../components/Admin/Title';
 import { FaStar } from "react-icons/fa";
 import { kConverter } from '../../lib/kConverter';
 import { FaCheck } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 const AddShows = () => {
   const currency = import.meta.env.VITE_CURRENCY
@@ -16,6 +17,34 @@ const AddShows = () => {
 
   const fetchNowPlayingMovies = async () =>{
     setNowPlayingMovies(dummyShowsData)
+  }
+
+  const handleDateTimeSelection =() =>{
+    if(!dateTimeInput) return;
+    const [date,time] = dateTimeInput.split("T");
+    if(!date || !time) return;
+
+    setDateTimeSelection((prev) => {
+      const times = prev[date] || [];
+      if(!times.includes(time)){
+        return {...prev,[date]: [...times,time]}
+      }
+      return prev;
+    })
+  }
+
+  const handleRemoveTime = (date, time)=>{
+    setDateTimeSelection((prev) =>{
+      const filteredTimes = prev[date].filter((t) => t !== time);
+      if(filteredTimes.length === 0){
+        const { [date]: _, ...rest } = prev;
+        return rest;
+      }
+      return {
+        ...prev,
+        [date]: filteredTimes,
+      }
+    })
   }
 
   useEffect(()=>{
@@ -59,7 +88,47 @@ const AddShows = () => {
           ))}
         </div>
       </div>
-      <div></div>
+      <div className='mt-8'>
+        <label className='block text-sm font-medium mb-2'>Show Price</label>
+        <div className='inline-flex items-center gap-2 border border-gray-600 px-3 py-2 rounded-md'>
+          <p className='text-gray-400 text-sm'>{currency}</p>
+          <input type="number" className="outline-none" min={0} value={showPrice} onChange={(e) => setShowPrice(e.target.value)} placeholder='Enter show price'/>
+
+        </div>
+      </div>
+      <div className='mt-6'>
+        <label className='block text-sm font-medium mb-2'>Select Date and Time</label>
+        <div className='inline-flex gap-5 border border-r-gray-600 p-1 pl-3 rounded-lg'>
+          <input type="datetime-local" value={dateTimeInput} onChange={(e) =>setDateTimeInput(e.target.value)} className='outline-none rounded-md'/>
+          <button className='bg-primary/80 text-white px-3 py-2 text-sm rounded-lg hover:bg-primary cursor-pointer'>Add Time</button>
+
+        </div>
+      </div>
+      
+      {Object.keys(dateTimeSelection).length > 0 && (
+        <div className='mt-6'>
+          <h2 className='mb-2'>Selected Date-Time</h2>
+          <ul className='space-y-3'>
+            {Object.entries(dateTimeSelection).map(([date,times]) => (
+              <li key={date}>
+                <div className='font-medium'>{date}</div>
+                <div className='flex flex-wrap gap-2 mt-1 text-sm'>
+                  {times.map((time)=>(
+                    <div key={time} className='border border-primary px-2 py-1 flex items-center rounded'>
+                      <span>{time}</span>
+                      <MdDelete  />
+                    </div>
+                  ))}
+                </div>
+
+              </li>
+            ))}
+          </ul>
+
+        </div>
+      )}
+      
+
     </>
   ) : (
     <Loading />

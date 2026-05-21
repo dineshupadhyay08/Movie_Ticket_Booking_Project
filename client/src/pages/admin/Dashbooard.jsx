984d603c647e6c8cfd785d55db2c66ac16dff791
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-21 23:59:56 +0530

    Fix Admin Dashboard

--- client/src/pages/admin/Dashbooard.jsx
+++ client/src/pages/admin/Dashbooard.jsx
@@ -7,7 +7,7 @@ import BlurCircle from '../../components/BlurCircle'
 
 const Dashbooard = () => {
 
-  const currency = import.meta.env.
+  const currency = import.meta.env.VITE_CURRENCY
 
 
   const [dashboardData, setDashboardData] = useState({

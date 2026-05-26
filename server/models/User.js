57b358b317c559bb36a75ba0395cf78a773cc6ef
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-26 19:30:48 +0530

    Add Schema

--- server/models/User.js
+++ server/models/User.js
@@ -6,3 +6,7 @@ const userSchema = new mongoose.Schema({
   email: {type: String, required: true, unique: true},
   image: {type: String, required: true},
 })
+
+const User = mongoose.model("User", userSchema);
+
+export default User;

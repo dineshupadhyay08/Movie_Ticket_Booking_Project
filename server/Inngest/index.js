8c9b31673d0e48201841b6d92a1e371781283e6f
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-28 16:56:38 +0530

    Add Clerk and Inngest

---
+++ server/Inngest/index.js
@@ -0,0 +1,6 @@
+import {Inngest} from "inngest";
+
+export const inngest = new Inngest({ id:"movie-ticket-counter" });
+
+
+export const functions = [];
--- server/server.js
+++ server/server.js
@@ -1,20 +1,33 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import connectDB from './configs/db.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+
+import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
 const Port = 3000;
 
-await connectDB()
+await connectDB();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("Welcome to the Movie Ticket Counter API!");
 });
 
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions,
+  }),
+);
+
 app.listen(Port, () => {
-  console.log(`Server is running on port http://localhost:${Port}`);
-}); 
+  console.log(`Server running at http://localhost:${Port}`);
+});

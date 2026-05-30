d6030b90493f6ef1c35d209c8d2e5a45ef331e0f
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-30 22:33:10 +0530

    Fix Live Runtime Error

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 
-import { inngest, functions } from "./inngest/index.js";
+import { inngest, functions } from "./Inngest/index.js";
 
 const app = express();
 const Port = 3000;

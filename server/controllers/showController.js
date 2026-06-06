88c3a582b3126507b9f1e756e4926c2f8857feed
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-06-06 15:38:10 +0530

    Add Backend Controller for data show

---
+++ server/controllers/showController.js
@@ -0,0 +1,84 @@
+import axios from "axios";
+import movies from "../models/Movies.js";
+import Show from "../models/Show.js";
+
+
+export const getNowPlayigMovies = async(req,res)=>{
+  try{
+    const {data} = await axios.get('https://api.themoviedb.org/3/movie/now_playing',{
+      headers: {Authorization: `Bearer ${process.env.TMDB_API_KEY}`}
+    })
+    
+    const movies = data.results;
+    res.json({success: true, movies: movies})
+    
+  }catch (error) {
+    console.log(error.message)
+    res.json({success: false,
+      message: 'Failed to fetch now playing movies',
+    })
+  }
+}
+
+export const addShow = async(req,res)=>{
+  try{
+    const {moviesId, showsInput, showPrice} = req.body;
+    let movie = await movies.findById(moviesId);
+    if(!movie){
+      const [movieDetailsResponse, moviesCreditsResponse] = await Promise.all([
+        axios.get(`https://api.themoviedb.org/3/movie/${moviesId}`, {
+          headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` },
+        }),
+        axios.get(`https://api.themoviedb.org/3/movie/${moviesId}/credits`, {
+          headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` },
+        }),
+      ]);
+      const movieApiData = movieDetailsResponse.data;
+      const moviesCreditsData = moviesCreditsResponse.data;
+
+      const movieDetails = {
+        _id: moviesId,
+        title: movieApiData.title,
+        overview: movieApiData.overview,
+        poster_path: movieApiData.poster_path,
+        backdrop_path: movieApiData.backdrop_path,
+        generes: movieApiData.generes,
+        casts: moviesCreditsData.cast,
+        release_date: movieApiData.release_date,
+        original_language: movieApiData.original_language,
+        tagline: movieApiData.tagline || "",
+        vote_average: movieApiData.vote_average,
+        runtime: movieApiData.runtime,
+      };
+
+      movies = await Movie.create(movieDetails);
+    }
+
+    const showToCreate = [];
+    showsInput.forEach(show=>{
+      const showDate = show.date;
+      show.time.forEach((time)=>{
+        const dateTimeString = `${showDate}T${time}`;
+        showToCreate.push({
+          movie: moviesId,
+          showdateTime: new Date(dateTimeString),
+          showPrice,
+          occupiedSeats: {}
+        })
+      })
+    })
+
+    if(showToCreate.length > 0){
+      await Show.insertMany(showToCreate);
+      res.json({
+        success: true,
+        message: 'Shows added successfully',
+      })
+    }
+
+
+  }catch(error){
+    console.log(error.message)
+    res.json({success:false, message: error.message})
+  }
+}

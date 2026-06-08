3eb0d5d29582aaa3ca8a354abb2669c7898e6811
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-06-08 23:57:14 +0530

    Booking Controller Add

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -21,7 +21,32 @@ const checkSeatsAvailability = async (req, res) => {
 export const createBooking = async(req,res)=>{
     try{
        const {userId} = req.auth();
-       
+       const {showId, selectedSeats} = req.body;
+       const {origin} = req.headers;
+
+       const isAvilable = await checkSeatsAvailability(showId, selectedSeats);
+       if(!isAvilable){
+        return res.json({
+            success: false,
+            message: "Selected seats are not available",
+          })
+       }
+
+       const showData = await Show.findById(showId).populate("movies");
+
+       const booking = await Booking.create({
+        user:userId,
+        show: showId,
+        amount: showData.showPrice * selectedSeats.length,
+        bookedSeats: selectedSeats,
+       })
+
+       selectedSeats.map((seat)=>{
+        showData.occupiedSeats[seat] = userId;
+       })
+
+       showData.markModified("occupiedSeats");
+
     }catch(error){
 
     }

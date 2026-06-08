54ba0d11e1c321ae6e07d5d740433fea994473a7
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-06-08 23:50:23 +0530

    Booking Controller Add

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,8 +1,28 @@
-const checkSeatsAvailability = (req, res) => {
+import Show from "../models/Show"
+
+const checkSeatsAvailability = async (req, res) => {
   try{
-    const showData = await Show
+    const showData = await Show.findById(showId)
+    if(!showData)return false;
+
+    const occupiedSeats = showData.occupiedSeats;
+
+    const isAnySeatTaken = selectedSeats.some(seat => occupiedSeats[seat]);
+
+    return !isAnySeatTaken;
 
   }catch(error){
-    
+    console.log(error.message);
+    return false;
   }
+}
+
+
+export const createBooking = async(req,res)=>{
+    try{
+       const {userId} = req.auth();
+       
+    }catch(error){
+
+    }
 }

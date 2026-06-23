23e589e512841634a66b1e2133270e0657f62a8d
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-06-23 22:50:30 +0530

    Add axios to connect backend data

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,36 +1,79 @@
-/* eslint react-refresh/only-export-components: 0 */
-import { createContext, useContext, useState } from "react";
-import axios from 'axios'
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { useAuth, useUser } from "@clerk/react";
+import {useLocation, useNavigate} from  "react-router-dom";
 
-axios.defaults.baseUrl = import.meta.env.VITE_BASE_URL
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
 
-const AppContext = createContext();
+export const AppContext = createContext()
 
-function AppProvider({ children }) {
+export const AppProvider = ({children})=>{
 
-  const [isAdmin,setIsAdmin] = useState(false);
+  const [isAdmin,setIsAdmin] = useState(false)
   const [show,setShow] = useState([])
   const [favoriteMovies,setFavoriteMovies] = useState([])
 
   const {user} = useUser
+  const {getToken} = useAuth()
+  const location = useLocation()
+  const navigate = useNavigate()
 
-  const fetchIsAdmin = async ()=>{
+  const fetchIsAdmin = async()=>{
     try{
-      const {data} = await axios.get('/api/admin/is-admin',{headers:{Authorization: `Bearer `}})
+      const {data} = await axios.get('/api/admin/is-admin',{headers:{Authorization: `Bearer ${await getToken()}`}})
+      setIsAdmin(data.isAdmin)
+      if(!data.isAdmin && location.pathname.startsWith('/admin')){
+          navigate('/')
+          toast.error('You are not authorized to access admin dashboard')
+      }
     }catch(error){
-      console.log(error)
+      console.error(error)
     }
   }
 
-  const value = {axios}
+  const fetchShows = async ()=>{
+    try{
+      const { data } = await axios.get('/api/show/all')
+      if(data.success){
+        setShow(data.shows)
+      }else{
+        toast.error(data.message)
+      }
+    }catch(error){
+      console.error(error)
+    }
+  }
 
+  const fetchFavoriteMovies = async()=>{
+    try{
+      const {data} = await axios.get('/api/user/favorites',{headers:{Authorization: `Bearer ${await getToken()}`}})
+      if(data.success){
+        setFavoriteMovies(data.movies)
+      }else{
+        toast.error(data.message)
+      }
+    }catch(error){
+      console.error(error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchShows()
+  },[])
+
+  useEffect(()=>{
+    if(user){
+      fetchIsAdmin()
+      fetchFavoriteMovies()
+    }
+  },[])
+
+  const value = {}
   return (
-    <AppContext.Provider value={value}>{children}</AppContext.Provider>
-  );
+    <AppContext.Provider value={value}>
+      {children}
+    </AppContext.Provider>
+  )
 }
 
-const useAppContext = () => useContext(AppContext);
-
-export { AppProvider, useAppContext };
-
-
+export const useAppContext = ()=> useContext(AppContext)

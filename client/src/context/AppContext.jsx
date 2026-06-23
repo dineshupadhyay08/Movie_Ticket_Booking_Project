bcff76cdc7d96e5cb7f4ca432350ce183ae4c1f3
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-06-23 17:03:26 +0530

    Add AuthProvider

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,14 +1,36 @@
-import { createContext, useContext } from "react";
+/* eslint react-refresh/only-export-components: 0 */
+import { createContext, useContext, useState } from "react";
+import axios from 'axios'
 
-export const AppContext = createContext()
+axios.defaults.baseUrl = import.meta.env.VITE_BASE_URL
 
-export const AppProvider = ({children})=>{
-  const value = {}
+const AppContext = createContext();
+
+function AppProvider({ children }) {
+
+  const [isAdmin,setIsAdmin] = useState(false);
+  const [show,setShow] = useState([])
+  const [favoriteMovies,setFavoriteMovies] = useState([])
+
+  const {user} = useUser
+
+  const fetchIsAdmin = async ()=>{
+    try{
+      const {data} = await axios.get('/api/admin/is-admin',{headers:{Authorization: `Bearer `}})
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  const value = {axios}
+
   return (
-    <AppContext.Provider value={value}>
-      {children}
-    </AppContext.Provider>
-  )
+    <AppContext.Provider value={value}>{children}</AppContext.Provider>
+  );
 }
 
-export const useAppContext = ()=> useContext(AppContext)
+const useAppContext = () => useContext(AppContext);
+
+export { AppProvider, useAppContext };
+
+

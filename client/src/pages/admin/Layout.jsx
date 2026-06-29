6883db9c6b031230cf57a709e0c493bea1aebdb9
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-06-29 23:33:02 +0530

    Fix App Context file improve admin page

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import AdminNabvar from '../../components/Admin/AdminNabvar'
-import AdminSidebar from '../../components/Admin/AdminSidebar'
-import { Outlet } from 'react-router'
+import React, { useEffect } from "react";
+import AdminNabvar from "../../components/Admin/AdminNabvar";
+import AdminSidebar from "../../components/Admin/AdminSidebar";
+import { Outlet } from "react-router-dom";
+import { useAppContext } from "../../context/AppContext";
+import Loading from "../../components/Loading";
 
 const Layout = () => {
-  return (
-    <>
-     <AdminNabvar/> 
-     <div className='flex'>
-      <AdminSidebar/>
-      <div className='flex-1 px-4 py-10 md:px-10 h-[calc(100vh-64px)] overflow-y-auto'>
-        <Outlet/>
-      </div>
-     </div>
-    </>
-  )
-}
+  const { isAdmin, fetchIsAdmin } = useAppContext();
 
-export default Layout
+  useEffect(() => {
+    fetchIsAdmin();
+  }, []);
+
+  return isAdmin ? (
+    <>
+      <AdminNabvar />
+      <div className="flex">
+        <AdminSidebar />
+        <div className="flex-1 px-4 py-10 md:px-10 h-[calc(100vh-64px)] overflow-y-auto">
+          <Outlet />
+        </div>
+      </div>
+    </>
+  ) : (
+    <Loading />
+  );
+};
+
+export default Layout;

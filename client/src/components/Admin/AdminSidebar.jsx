80eacc4d59b3a074dbec079e7b3a679efe75a7be
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-07-05 18:15:36 +0530

    Add addShaw as a Admin

--- client/src/components/Admin/AdminSidebar.jsx
+++ client/src/components/Admin/AdminSidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
 import { LayoutDashboardIcon, ListCollapseIcon, ListIcon, PlusSquareIcon } from 'lucide-react'
-import { NavLink } from 'react-router'
+import { NavLink } from "react-router-dom";
 
 const AdminSidebar = () => {
 

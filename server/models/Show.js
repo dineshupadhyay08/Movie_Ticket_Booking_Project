02968190dd06d0a64aa0d3d35f2f874286636b36
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-07-31 17:48:10 +0530

    Fix models booking and shows

--- server/models/Show.js
+++ server/models/Show.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const showSchema = new mongoose.Schema({
-  movies: {type: String, required: true},
+  movies: {type: String, ref: "Movie", required: true},
   showDateTime: {type: Date, required: true},
   showPrice: {type: Number, required: true},
   occupiedSeats: {type: Object, default: {}},

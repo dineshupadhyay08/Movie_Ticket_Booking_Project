fdf307de4e2922dcaa268b81c1b87ab092570bae
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-07-31 17:47:21 +0530

    Fix models booking and shows

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema(
   {
-    user: { type: String, required: true },
+    user: { type: String, ref: "User", required: true },
     show: { type: String, required: true, ref: "Show" },
     amount: { type: Number, required: true },
     bookedSeats: { type: Array, required: true },
